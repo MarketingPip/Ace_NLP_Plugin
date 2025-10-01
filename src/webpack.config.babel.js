b7caa43f8d90aa2efb4c37e97cb840552bc857f1
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-01 04:11:41 -0400

    Update webpack.config.babel.js

--- src/webpack.config.babel.js
+++ src/webpack.config.babel.js
@@ -1,113 +1,119 @@
-// webpack.config.js
-    // Awaited dynamic imports for ES Modules
-    const fs = await import('fs');
-    const path = await import('path');
-    
-    const packageData = await import('./license.config.js');
-    
-    const TerserPlugin = await import('terser-webpack-plugin');
-
-if (typeof require !== 'undefined') {
-    // CommonJS imports
-    const fs = require('fs');
-    const path = require('path');
-    
-    const packageData = require('./license.config.js');
-    
-    const TerserPlugin = require('terser-webpack-plugin');
+async function loadModules() {
+  let fs, path, packageData, TerserPlugin;
+  
+  // Dynamically load modules
+  if (typeof module !== 'undefined' && module.exports) {
+    // CommonJS: Use dynamic imports instead of require()
+    fs = await import('fs');
+    path = await import('path');
+    packageData = await import('./license.config.js');
+    TerserPlugin = (await import('terser-webpack-plugin')).default;
   } else {
-
-    
-    // Use fs, path, packageData, and TerserPlugin here
+    // ESM environment: Use dynamic imports directly
+    fs = await import('fs');
+    path = await import('path');
+    packageData = await import('./license.config.js');
+    TerserPlugin = (await import('terser-webpack-plugin')).default;
   }
- 
-class AddLicenseAfterTerserPlugin {
+
+  // Define plugins and classes after modules are loaded
+  class AddLicenseAfterTerserPlugin {
     constructor(options) {
-        this.options = options;
+      this.options = options;
     }
 
     apply(compiler) {
-        compiler.hooks.afterEmit.tap('AddLicenseAfterTerserPlugin', compilation => {
-            const outputPath = this.options.outputPath || compiler.options.output.path;
-            const outputFileName = this.options.outputFileName || compiler.options.output.filename;
+      compiler.hooks.afterEmit.tap('AddLicenseAfterTerserPlugin', compilation => {
+        const outputPath = this.options.outputPath || compiler.options.output.path;
+        const outputFileName = this.options.outputFileName || compiler.options.output.filename;
 
-            // Construct the full path to the output file
-            const outputFilePath = path.join(outputPath, outputFileName);
+        // Construct the full path to the output file
+        const outputFilePath = path.join(outputPath, outputFileName);
 
-            // Read the existing file content
-            fs.readFile(outputFilePath, 'utf8', (err, data) => {
-                if (err) throw err;
+        // Read the existing file content
+        fs.readFile(outputFilePath, 'utf8', (err, data) => {
+          if (err) throw err;
 
-                // Add your license text after minification (Terser)
-                const licenseText = `${packageData.LICENSE} `;
+          // Add your license text after minification (Terser)
+          const licenseText = `${packageData.LICENSE} `;
 
-                // Append license text to the existing file content
-                const newContent = licenseText + data;
+          // Append license text to the existing file content
+          const newContent = licenseText + data;
 
-                // Write back the modified content to the output file
-                fs.writeFile(outputFilePath, newContent, 'utf8', err => {
-                    if (err) throw err;
-                    console.log(`License added to ${outputFileName}`);
-                });
-            });
+          // Write back the modified content to the output file
+          fs.writeFile(outputFilePath, newContent, 'utf8', err => {
+            if (err) throw err;
+            console.log(`License added to ${outputFileName}`);
+          });
         });
+      });
     }
-}
+  }
 
-// taken from https://github.com/webpack/webpack/issues/12506#issuecomment-1360810560
-class RemoveLicenseFilePlugin {
+  // taken from https://github.com/webpack/webpack/issues/12506#issuecomment-1360810560
+  class RemoveLicenseFilePlugin {
     apply(compiler) {
-        compiler.hooks.emit.tap("RemoveLicenseFilePlugin", (compilation) => {
-            // compliation has assets to output
-            // console.log(compilation.assets);
-            for (let name in compilation.assets) {
-                if (name.endsWith("LICENSE.txt")) {
-                    delete compilation.assets[name];
-                }
-            }
-        });
+      compiler.hooks.emit.tap("RemoveLicenseFilePlugin", (compilation) => {
+        for (let name in compilation.assets) {
+          if (name.endsWith("LICENSE.txt")) {
+            delete compilation.assets[name];
+          }
+        }
+      });
     }
-}
+  }
 
-
-module.exports = {
-  entry: `./src/${packageData.FILENAME}.js`,
-  output: {
-    path: path.resolve(__dirname, '..', 'dist'),
-    filename: packageData.main.split("/").pop(),
-    library: {
-      type: 'module',
+  // Now return the Webpack config after modules are loaded
+  return {
+    entry: `./src/${packageData.FILENAME}.js`,
+    output: {
+      path: path.resolve(__dirname, '..', 'dist'),
+      filename: packageData.main.split("/").pop(),
+      library: {
+        type: 'module',
+      },
     },
-  },
-  experiments: {
-    outputModule: true,
-  },
-  optimization: {
-    minimizer: [new TerserPlugin({
-      extractComments: false,
-    })],
-  },
-  plugins: [new RemoveLicenseFilePlugin(),  new AddLicenseAfterTerserPlugin({
-            // Additional options can be passed here if needed
-        })],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+    experiments: {
+      outputModule: true,
+    },
+    optimization: {
+      minimizer: [new TerserPlugin({
+        extractComments: false,
+      })],
+    },
+    plugins: [
+      new RemoveLicenseFilePlugin(),
+      new AddLicenseAfterTerserPlugin({
+        // Additional options can be passed here if needed
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-   externals: {
-    ace: 'ace', // ace will be assumed to be available globally
-  },
-};
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    externals: {
+      ace: 'ace', // ace will be assumed to be available globally
+    },
+  };
+}
+
+// Call the async function and use its result to create the Webpack config
+loadModules().then((webpackConfig) => {
+  // Export the Webpack config after all async modules have been loaded
+  module.exports = webpackConfig;
+}).catch(err => {
+  console.error("Error loading modules:", err);
+});
