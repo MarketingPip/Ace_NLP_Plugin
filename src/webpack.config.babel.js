83da0af68f8325ef74e87e6553c75624a6cbd153
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-10-04 00:27:54 -0400

    Update webpack.config.babel.js

--- src/webpack.config.babel.js
+++ src/webpack.config.babel.js
@@ -1,80 +1,74 @@
 // webpack.config.js
-import fs from 'fs';
-import path from 'path';
+// webpack.config.js
 
-import packageData from './license.config.js';
+// Determine if we are in a CommonJS environment
+const isRequireEnv = typeof require !== 'undefined' && typeof module !== 'undefined';
 
-import TerserPlugin from 'terser-webpack-plugin';
- 
+// Conditionally load Node modules
+const path = isRequireEnv ? require('path') : await import('path').then(m => m.default || m);
+const fs = isRequireEnv ? require('fs') : await import('fs').then(m => m.default || m);
+
+// Conditionally load other modules
+const packageData = isRequireEnv
+  ? require('./license.config.js')
+  : (await import('./license.config.js')).default;
+
+const TerserPlugin = isRequireEnv
+  ? require('terser-webpack-plugin')
+  : (await import('terser-webpack-plugin')).default;
+
+// Plugin to add license after minification
 class AddLicenseAfterTerserPlugin {
-    constructor(options) {
-        this.options = options;
-    }
+  constructor(options = {}) {
+    this.options = options;
+  }
 
-    apply(compiler) {
-        compiler.hooks.afterEmit.tap('AddLicenseAfterTerserPlugin', compilation => {
-            const outputPath = this.options.outputPath || compiler.options.output.path;
-            const outputFileName = this.options.outputFileName || compiler.options.output.filename;
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('AddLicenseAfterTerserPlugin', (compilation) => {
+      const outputPath = this.options.outputPath || compiler.options.output.path;
+      const outputFileName = this.options.outputFileName || compiler.options.output.filename;
+      const outputFilePath = path.join(outputPath, outputFileName);
 
-            // Construct the full path to the output file
-            const outputFilePath = path.join(outputPath, outputFileName);
+      fs.readFile(outputFilePath, 'utf8', (err, data) => {
+        if (err) throw err;
+        const licenseText = `${packageData.LICENSE} `;
+        const newContent = licenseText + data;
 
-            // Read the existing file content
-            fs.readFile(outputFilePath, 'utf8', (err, data) => {
-                if (err) throw err;
-
-                // Add your license text after minification (Terser)
-                const licenseText = `${packageData.LICENSE} `;
-
-                // Append license text to the existing file content
-                const newContent = licenseText + data;
-
-                // Write back the modified content to the output file
-                fs.writeFile(outputFilePath, newContent, 'utf8', err => {
-                    if (err) throw err;
-                    console.log(`License added to ${outputFileName}`);
-                });
-            });
+        fs.writeFile(outputFilePath, newContent, 'utf8', (err) => {
+          if (err) throw err;
+          console.log(`License added to ${outputFileName}`);
         });
-    }
+      });
+    });
+  }
 }
 
-// taken from https://github.com/webpack/webpack/issues/12506#issuecomment-1360810560
+// Plugin to remove LICENSE.txt from emitted assets
 class RemoveLicenseFilePlugin {
-    apply(compiler) {
-        compiler.hooks.emit.tap("RemoveLicenseFilePlugin", (compilation) => {
-            // compliation has assets to output
-            // console.log(compilation.assets);
-            for (let name in compilation.assets) {
-                if (name.endsWith("LICENSE.txt")) {
-                    delete compilation.assets[name];
-                }
-            }
-        });
-    }
+  apply(compiler) {
+    compiler.hooks.emit.tap('RemoveLicenseFilePlugin', (compilation) => {
+      for (const name in compilation.assets) {
+        if (name.endsWith('LICENSE.txt')) {
+          delete compilation.assets[name];
+        }
+      }
+    });
+  }
 }
 
-
-module.exports = {
+// Export Webpack config (async to support ES module conditional imports)
+export default async () => ({
   entry: `./src/${packageData.FILENAME}.js`,
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
-    filename: packageData.main.split("/").pop(),
-    library: {
-      type: 'module',
-    },
+    filename: packageData.main.split('/').pop(),
+    library: { type: 'module' },
   },
-  experiments: {
-    outputModule: true,
-  },
+  experiments: { outputModule: true },
   optimization: {
-    minimizer: [new TerserPlugin({
-      extractComments: false,
-    })],
+    minimizer: [new TerserPlugin({ extractComments: false })],
   },
-  plugins: [new RemoveLicenseFilePlugin(),  new AddLicenseAfterTerserPlugin({
-            // Additional options can be passed here if needed
-        })],
+  plugins: [new RemoveLicenseFilePlugin(), new AddLicenseAfterTerserPlugin()],
   module: {
     rules: [
       {
@@ -82,17 +76,12 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+          options: { presets: ['@babel/preset-env'] },
         },
       },
     ],
   },
-  resolve: {
-    extensions: ['.js'],
-  },
-   externals: {
-    ace: 'ace', // ace will be assumed to be available globally
-  },
-};
+  resolve: { extensions: ['.js'] },
+  externals: { ace: 'ace' },
+});
+
